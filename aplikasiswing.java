19736094ac39a06f33151b7a3f42a1f6f92ea8ba
fakhitaaa <91232506+fakhitaaa@users.noreply.github.com>
2021-11-11 18:49:29 +0800

    Add files via upload

---
+++ aplikasiswing.java
@@ -0,0 +1,38 @@
+package aplikasiswing;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public abstract class aplikasiswing implements ActionListener{
+	
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("I am a JFrame"); frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(20, 30, 300, 100);
+		frame.getContentPane().setLayout(null);
+		
+		JButton butt = new JButton("Click Me");
+		frame.getContentPane().add(butt);
+		butt.setBounds(20, 20, 200, 20);
+		
+		aplikasiswing app = new aplikasiswing() {};
+		
+		app.label =  new JLabel ("Nama saya adalah");
+		app.label.setBounds(20, 40, 200, 20);
+		frame.getContentPane().add(app.label);
+		
+		butt.addActionListener(app);
+		frame.setVisible(true);		
+	}
+	public void actionPerformed(ActionEvent e){
+		label.setText("Fakhita Imud");
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+	JLabel label;
+}
